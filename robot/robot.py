bb620348687114ebcdf78d8baf9e62359d7c8c0f
zhangzhi <781029588@qq.com>
2022-02-24 18:57:18 +0800

    fix

--- robot/robot.py
+++ robot/robot.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-import psycopg2
+import redis
 from werobot import WeRoBot
 from werobot.config import Config
 from werobot.messages.messages import TextMessage
-from werobot.session.postgresqlstorage import PostgreSQLStorage
+from werobot.session.redisstorage import RedisStorage
 
 from mp_game.settings import WECHAT_SECRET, WECHAT_APPID, WECHAT_TOKEN
 from robot.controllers import session_ctrl
 from robot.controllers.text import user
 from robot.controllers.text import study
 
-
-conn = psycopg2.connect(host='42.193.105.229', port='5432', database='mp_game', user='mp_game', password='hsNABfdeL8b3')
-session_storage = PostgreSQLStorage(conn)
+db = redis.Redis()
+session_storage = RedisStorage(db, prefix="mp_game")
 
 
 config = Config(
